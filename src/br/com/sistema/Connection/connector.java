a1b911cd288544e4e02b6a1af221660c3084857f
Felipe Ávila <79549074+FelipeGFA@users.noreply.github.com>
2023-11-21 16:34:34 -0300

    SistemaDeEstoque

--- src/br/com/sistema/Connection/connector.java
+++ src/br/com/sistema/Connection/connector.java
@@ -3,29 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.sistema.Connection;
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+import java.sql.*;
+/**
+ *
+ * @author Felipe Firmino
+ */
 public class connector {
-
-    public static java.sql.Connection connect() {
-
-        java.sql.Connection conn = null;
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = (java.sql.Connection) DriverManager.getConnection(
-                    "sql10.freesqldatabase.com",
-                    "sql10662533",
-                    "9VUyNsa1k3");
-            
-            System.out.println("Conexao bem sucedida");
+    public static Connection Conector(){
+    Connection connection = null;
+     //the path for the driver in the lib, in this app it was used the one below
+    String driver = "com.mysql.cj.jdbc.Driver";
+    //where the data base is installed, in my case it was in my own machine 
+    String url = "jdbc:mysql://sql10.freesqldatabase.com:3306/sql10662533";
+    String user = "sql10662533";
+    String password = "9VUyNsa1k3";
+    
+    try {
+     Class.forName(driver);
+     connection = DriverManager.getConnection(url,user,password);
+     return connection;
+    } catch (Exception e) {
+        System.out.println("erro na conexao");
+            System.out.println(e.getMessage());                 
+            return null;
         }
-
-        catch (ClassNotFoundException | SQLException e) {
-            System.out.println(e);
-        }
-        return conn;
     }
 }
